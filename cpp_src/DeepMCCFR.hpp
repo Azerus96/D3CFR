1cf135450d7d00c36dfc217e3d67b4f80c9c87c0
Azerus96 <gencinski1996@gmail.com>
2025-06-27 21:48:03 +0300

    Update DeepMCCFR.hpp
    РЕАЛИЗОВАЛИ ЛОГИКУ НЕИЗВЕСТНОСТИ СБРОСА КАРТ ДРУГ ДРУГА

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include "game_state.hpp"
 #include "hand_evaluator.hpp"
-#include "request_manager.hpp" // НОВЫЙ INCLUDE
+#include "request_manager.hpp"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <vector>
 #include <map>
-#include <memory> // для std::shared_ptr
+#include <memory>
 
 namespace py = pybind11;
 
@@ -20,17 +20,17 @@ struct TrainingSample {
 
 class DeepMCCFR {
 public:
-    // Конструктор теперь принимает указатель на менеджер запросов
     DeepMCCFR(std::shared_ptr<RequestManager> manager, size_t action_limit);
     std::vector<TrainingSample> run_traversal();
 
 private:
     HandEvaluator evaluator_;
-    std::shared_ptr<RequestManager> request_manager_; // Указатель на общий менеджер
+    std::shared_ptr<RequestManager> request_manager_;
     size_t action_limit_;
 
     std::map<int, float> traverse(GameState state, int traversing_player, std::vector<TrainingSample>& samples);
-    std::vector<float> featurize(const GameState& state);
+    // ИЗМЕНЕНО: Сигнатура featurize теперь включает player_view
+    std::vector<float> featurize(const GameState& state, int player_view);
 };
 
 } // namespace ofc
