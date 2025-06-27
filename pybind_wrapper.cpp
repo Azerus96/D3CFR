bf5a05ea69ee2e4214df899601bc217977862009
Azerus96 <gencinski1996@gmail.com>
2025-06-27 09:34:20 +0300

    Update pybind_wrapper.cpp
    Вернули код до турбо для диагностики

--- pybind_wrapper.cpp
+++ pybind_wrapper.cpp
@@ -2,12 +2,11 @@
 #include <pybind11/stl.h>
 #include <pybind11/functional.h>
 #include "cpp_src/DeepMCCFR.hpp"
-#include "cpp_src/request_manager.hpp"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(ofc_engine, m) {
-    m.doc() = "OFC Pineapple DeepMCCFR Engine with Batched Inference";
+    m.doc() = "OFC Pineapple DeepMCCFR Engine";
 
     py::class_<ofc::TrainingSample>(m, "TrainingSample")
         .def(py::init<>())
@@ -15,25 +14,7 @@ PYBIND11_MODULE(ofc_engine, m) {
         .def_readwrite("target_regrets", &ofc::TrainingSample::target_regrets)
         .def_readwrite("num_actions", &ofc::TrainingSample::num_actions);
 
-    // Биндинги для структур запросов и результатов
-    py::class_<ofc::PredictionRequest>(m, "PredictionRequest")
-        .def_readonly("id", &ofc::PredictionRequest::id)
-        .def_readonly("infoset_vector", &ofc::PredictionRequest::infoset_vector)
-        .def_readonly("num_actions", &ofc::PredictionRequest::num_actions);
-
-    py::class_<ofc::PredictionResult>(m, "PredictionResult")
-        .def(py::init<>())
-        .def_readwrite("id", &ofc::PredictionResult::id)
-        .def_readwrite("regrets", &ofc::PredictionResult::regrets);
-
-    // Биндинг для менеджера запросов
-    py::class_<ofc::RequestManager, std::shared_ptr<ofc::RequestManager>>(m, "RequestManager")
-        .def(py::init<>())
-        .def("get_requests", &ofc::RequestManager::get_requests, py::arg("max_batch_size"))
-        .def("post_results", &ofc::RequestManager::post_results);
-
-    // Биндинг для солвера, который теперь принимает указатель на менеджер
     py::class_<ofc::DeepMCCFR>(m, "DeepMCCFR")
-        .def(py::init<std::shared_ptr<ofc::RequestManager>, size_t>())
+        .def(py::init<py::function, size_t>())
         .def("run_traversal", &ofc::DeepMCCFR::run_traversal, "Runs one full game traversal and returns training data.");
 }
