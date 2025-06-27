2fa63434c9bd53d5f16a9cf56929e50da04b9eef
Azerus96 <gencinski1996@gmail.com>
2025-06-27 07:46:39 +0300

    Update replay_buffer.py
    Сэмплинг

--- python_src/replay_buffer.py
+++ python_src/replay_buffer.py
@@ -1,25 +1,25 @@
-# mccfr_ofc-main/python_src/replay_buffer.py
-
 import random
 from collections import deque
 import numpy as np
 import torch
 
 class ReplayBuffer:
-    def __init__(self, capacity):
+    def __init__(self, capacity, action_limit):
         self.buffer = deque(maxlen=capacity)
+        self.action_limit = action_limit
 
     def push(self, infoset_vector, target_regrets, num_actions):
-        # Убедимся, что вектор сожалений имеет правильную длину
-        padded_regrets = np.zeros(200, dtype=np.float32) # 200 - ACTION_LIMIT
-        padded_regrets[:num_actions] = target_regrets
+        padded_regrets = np.zeros(self.action_limit, dtype=np.float32)
+        if num_actions > 0:
+            padded_regrets[:num_actions] = target_regrets
         self.buffer.append((infoset_vector, padded_regrets, num_actions))
 
     def sample(self, batch_size):
+        if len(self.buffer) < batch_size:
+            return None, None, None
         samples = random.sample(self.buffer, batch_size)
         infosets, targets, num_actions_list = zip(*samples)
         
-        # Преобразуем в тензоры PyTorch
         infosets_tensor = torch.FloatTensor(np.array(infosets))
         targets_tensor = torch.FloatTensor(np.array(targets))
         
