c5ea3288c0250fca60bccbf0142e5438b8d6bc3e
Azerus96 <gencinski1996@gmail.com>
2025-06-27 10:29:31 +0300

    Update DeepMCCFR.hpp
    Вернул турбонаддув

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include "game_state.hpp"
 #include "hand_evaluator.hpp"
+#include "request_manager.hpp" // НОВЫЙ INCLUDE
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <vector>
 #include <map>
+#include <memory> // для std::shared_ptr
 
 namespace py = pybind11;
 
@@ -18,12 +20,13 @@ struct TrainingSample {
 
 class DeepMCCFR {
 public:
-    DeepMCCFR(py::function predict_callback, size_t action_limit);
+    // Конструктор теперь принимает указатель на менеджер запросов
+    DeepMCCFR(std::shared_ptr<RequestManager> manager, size_t action_limit);
     std::vector<TrainingSample> run_traversal();
 
 private:
     HandEvaluator evaluator_;
-    py::function predict_callback_;
+    std::shared_ptr<RequestManager> request_manager_; // Указатель на общий менеджер
     size_t action_limit_;
 
     std::map<int, float> traverse(GameState state, int traversing_player, std::vector<TrainingSample>& samples);
