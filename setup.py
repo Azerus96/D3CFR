c84c2e3b61165e25707fae7544a7977b2b0807d7
Azerus96 <gencinski1996@gmail.com>
2025-06-28 04:13:20 +0300

    Update setup.py
    Либторч

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# mccfr_ofc-main/setup.py
+# D2CFR-main/setup.py
 
 from setuptools import setup, Extension
 import pybind11
@@ -6,18 +6,23 @@ import glob
 import os
 import sys
 
-# Определяем аргументы компилятора
+# --- LIBTORCH CONFIGURATION ---
+# Set this environment variable to the path of your unzipped libtorch directory
+# For example: export LIBTORCH_PATH=/path/to/libtorch
+libtorch_path = os.getenv('LIBTORCH_PATH')
+if not libtorch_path:
+    raise EnvironmentError("LIBTORCH_PATH environment variable not set. Please set it to your LibTorch directory.")
+
+# --- COMPILER ARGUMENTS ---
 cpp_args = ['-std=c++17', '-O3', '-fopenmp']
+link_args = ['-fopenmp']
 
-# Для macOS может потребоваться другой флаг OpenMP и библиотеки
+# For macOS
 if sys.platform == 'darwin':
-    cpp_args = ['-std=c++17', '-O3', '-Xpreprocessor', '-fopenmp', '-lomp']
+    cpp_args = ['-std=c++17', '-O3', '-Xpreprocessor', '-fopenmp']
     link_args = ['-lomp']
-else: # Для Linux (Colab)
-    link_args = ['-fopenmp']
 
-
-# Находим все C++ исходники
+# --- SOURCE FILES ---
 ompeval_sources = glob.glob("cpp_src/ompeval/omp/*.cpp")
 ofc_sources = [
     "cpp_src/game_state.cpp",
@@ -26,24 +31,35 @@ ofc_sources = [
 
 ext_modules = [
     Extension(
-        'ofc_engine', # Имя модуля, которое будем импортировать в Python
+        'ofc_engine',
         ['pybind_wrapper.cpp'] + ompeval_sources + ofc_sources,
         include_dirs=[
             pybind11.get_include(),
             "cpp_src",
-            "cpp_src/ompeval"
+            "cpp_src/ompeval",
+            # ADDED: LibTorch include paths
+            os.path.join(libtorch_path, 'include'),
+            os.path.join(libtorch_path, 'include', 'torch', 'csrc', 'api', 'include'),
         ],
         language='c++',
         extra_compile_args=cpp_args,
-        extra_link_args=link_args
+        # MODIFIED: Linking arguments for LibTorch
+        library_dirs=[os.path.join(libtorch_path, 'lib')],
+        extra_link_args=link_args + [
+            '-ltorch', 
+            '-ltorch_cpu', 
+            '-lc10',
+            # This is important for the dynamic linker to find the .so files at runtime
+            f'-Wl,-rpath,{os.path.join(libtorch_path, "lib")}'
+        ]
     ),
 ]
 
 setup(
     name="ofc_engine",
-    version="2.0.0",
+    version="3.0.0", # Version bump!
     author="Azerus96 & AI Solver",
-    description="Deep MCCFR solver for Pineapple OFC poker.",
+    description="Deep MCCFR solver for Pineapple OFC poker with C++ inference.",
     ext_modules=ext_modules,
     zip_safe=False,
 )
