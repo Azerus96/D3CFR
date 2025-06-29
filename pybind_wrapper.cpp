668f5c5b2319ee7763e7d8263a128fcc48742560
Azerus96 <gencinski1996@gmail.com>
2025-06-29 19:24:25 +0300

    Update pybind_wrapper.cpp

--- pybind_wrapper.cpp
+++ pybind_wrapper.cpp
@@ -1,4 +1,4 @@
-// D2CFR-main/pybind_wrapper.cpp (ФИНАЛЬНЫЙ ТЕСТ)
+// D2CFR-main/pybind_wrapper.cpp (ФИНАЛЬНАЯ ОПТИМИЗИРОВАННАЯ ВЕРСИЯ)
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -14,13 +14,24 @@ PYBIND11_MODULE(ofc_engine, m) {
     py::class_<ofc::SharedReplayBuffer>(m, "SharedReplayBuffer")
         .def(py::init<uint64_t, int>(), py::arg("capacity"), py::arg("action_limit"))
         .def("get_count", &ofc::SharedReplayBuffer::get_count)
+        .def("get_max_actions", &ofc::SharedReplayBuffer::get_max_actions)
         .def("sample", [](ofc::SharedReplayBuffer &buffer, int batch_size) {
             int action_limit = buffer.get_max_actions();
-            auto infosets_np = py::array_t<float>({batch_size, ofc::INFOSET_SIZE});
-            auto regrets_np = py::array_t<float>({batch_size, action_limit});
-            buffer.sample(batch_size, static_cast<float*>(infosets_np.request().ptr), static_cast<float*>(regrets_np.request().ptr));
+            
+            auto infosets_np = py::array_t<float>(batch_size * ofc::INFOSET_SIZE);
+            auto regrets_np = py::array_t<float>(batch_size * action_limit);
+
+            buffer.sample(
+                batch_size, 
+                static_cast<float*>(infosets_np.request().ptr), 
+                static_cast<float*>(regrets_np.request().ptr)
+            );
+            
+            infosets_np.resize({batch_size, ofc::INFOSET_SIZE});
+            regrets_np.resize({batch_size, action_limit});
+
             return std::make_pair(infosets_np, regrets_np);
-        });
+        }, py::arg("batch_size"));
 
     py::class_<ofc::DeepMCCFR>(m, "DeepMCCFR")
         .def(py::init<const std::string&, size_t, ofc::SharedReplayBuffer*>(), 
