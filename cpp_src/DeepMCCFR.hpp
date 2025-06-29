d63f17d8b8087ef284e8ab5f2f8b978de5cb088c
Azerus96 <gencinski1996@gmail.com>
2025-06-29 19:21:43 +0300

    Update DeepMCCFR.hpp

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -1,4 +1,4 @@
-// D2CFR-main/cpp_src/DeepMCCFR.hpp (ФИНАЛЬНЫЙ ТЕСТ)
+// D2CFR-main/cpp_src/DeepMCCFR.hpp (ФИНАЛЬНАЯ ОПТИМИЗИРОВАННАЯ ВЕРСИЯ)
 
 #pragma once
 #include "game_state.hpp"
@@ -15,8 +15,6 @@ class DeepMCCFR {
 public:
     DeepMCCFR(const std::string& model_path, size_t action_limit, SharedReplayBuffer* buffer);
     
-    // Упрощенный метод для профайлинга, который ничего не возвращает.
-    // Мы будем измерять время и количество сэмплов на стороне Python.
     void run_traversal();
 
 private:
@@ -26,7 +24,6 @@ private:
     SharedReplayBuffer* replay_buffer_; 
     size_t action_limit_;
 
-    // Убираем stats, он нам больше не нужен
     std::map<int, float> traverse(GameState& state, int traversing_player);
     std::vector<float> featurize(const GameState& state, int player_view);
 };
