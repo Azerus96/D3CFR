7bd12377e8dfe204a5e3ae36036fb50772f9103f
Azerus96 <gencinski1996@gmail.com>
2025-06-30 04:56:09 +0300

    Update DeepMCCFR.hpp
    Внедрили пакетный инференс

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -1,10 +1,10 @@
-// D2CFR-main/cpp_src/DeepMCCFR.hpp (ВЕРСИЯ 5.0)
+// D2CFR-main/cpp_src/DeepMCCFR.hpp (ВЕРСИЯ 7.0)
 
 #pragma once
 #include "game_state.hpp"
 #include "hand_evaluator.hpp"
 #include "SharedReplayBuffer.hpp"
-#include <torch/script.h>
+#include "InferenceQueue.hpp" // <-- Подключаем новый файл
 #include <vector>
 #include <map>
 #include <memory>
@@ -13,15 +13,15 @@ namespace ofc {
 
 class DeepMCCFR {
 public:
-    DeepMCCFR(const std::string& model_path, size_t action_limit, SharedReplayBuffer* buffer);
+    // Конструктор теперь принимает указатели на общие ресурсы
+    DeepMCCFR(size_t action_limit, SharedReplayBuffer* buffer, InferenceQueue* queue);
     
     void run_traversal();
 
 private:
     HandEvaluator evaluator_;
-    torch::jit::script::Module model_; 
-    torch::Device device_;
     SharedReplayBuffer* replay_buffer_; 
+    InferenceQueue* inference_queue_; // <-- Указатель на очередь инференса
     size_t action_limit_;
 
     std::map<int, float> traverse(GameState& state, int traversing_player);
