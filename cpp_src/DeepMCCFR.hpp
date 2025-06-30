5bab8388ef8a96c6aa7ee1e5ac9665c9d2403797
Azerus96 <gencinski1996@gmail.com>
2025-06-30 04:04:52 +0300

    Update DeepMCCFR.hpp
    Проверка гипотезы инициализации

--- cpp_src/DeepMCCFR.hpp
+++ cpp_src/DeepMCCFR.hpp
@@ -1,31 +1,28 @@
-// D2CFR-main/cpp_src/DeepMCCFR.hpp (ВЕРСИЯ 6.0 - MULTIPROCESSING)
+// D2CFR-main/cpp_src/DeepMCCFR.hpp (ВЕРСИЯ 5.0)
 
 #pragma once
 #include "game_state.hpp"
 #include "hand_evaluator.hpp"
+#include "SharedReplayBuffer.hpp"
 #include <torch/script.h>
 #include <vector>
 #include <map>
 #include <memory>
-#include <pybind11/pybind11.h>
 
 namespace ofc {
 
 class DeepMCCFR {
 public:
-    DeepMCCFR(const std::string& model_path, size_t action_limit, pybind11::object queue);
+    DeepMCCFR(const std::string& model_path, size_t action_limit, SharedReplayBuffer* buffer);
     
     void run_traversal();
 
 private:
-    void push_to_queue(const std::vector<float>& infoset, const std::vector<float>& regrets, int num_actions);
-
     HandEvaluator evaluator_;
     torch::jit::script::Module model_; 
     torch::Device device_;
+    SharedReplayBuffer* replay_buffer_; 
     size_t action_limit_;
-    
-    pybind11::object queue_;
 
     std::map<int, float> traverse(GameState& state, int traversing_player);
     std::vector<float> featurize(const GameState& state, int player_view);
